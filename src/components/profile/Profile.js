ef23406dec0416d4c593ede69150c7b421e75e9e
Zach D'Amico <zachdamico90@gmail.com>
2023-09-16 17:03:38 -0500

    user showing on page

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,2 +1,4 @@
 //import GetFavorites
 import "./Profile.css";
+
+//want to import user objects and favorite object data here and do JSX
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 import { getRecordUsers } from "../../services/userFetchService";
+import "./Users.css";
 
 //need props
 //how do i get the recordUser info here? - useState? map? do i need more than 1 user?
-export const User = () => {
+export const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getRecordUsers().then((userArray) => {
+      setUsers(userArray);
+    });
+  }, []);
   return (
-    <div className="user">
-      <div>
-        <div>{recordUser.name}</div>
-      </div>
+    <div className="users">
+      {users.map((userObj) => {
+        return (
+          <div key={userObj.id}>
+            <p>{userObj.name}</p>
+            <p>{userObj.userName}</p>
+            <p>{userObj.email}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
